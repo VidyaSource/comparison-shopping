b6a6cc64e618e97dc18390a610df4b7fa7d3e20b
Neil Chaudhuri <neil@vidyasource.com>
2013-09-28 00:53:58 -0400

    Exception handling

--- src/main/java/com/vidyasource/tutorial/comparison/MakeComparator.java
+++ src/main/java/com/vidyasource/tutorial/comparison/MakeComparator.java
@@ -6,6 +6,12 @@ public class MakeComparator implements Comparator<Car> {
 
     @Override
     public int compare(Car car, Car car2) {
+        if (car == null || car2 == null) {
+            throw new NullPointerException("Failed attempt to compare " + car + " to " + car2);
+        }
+        else if (!car.getClass().equals(car2.getClass())) {
+            throw new ClassCastException("Possible ClassLoader issue. Failed attempt to compare " + car + " to " + car2);
+        }
         return car.getMake().compareTo(car2.getMake());
     }
 
